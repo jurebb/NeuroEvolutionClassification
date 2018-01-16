c06cf74c215f9a1f135960638818e0d7c354f7ad
Jure Baban <Jure Baban@DESKTOP-QSMARQ1>
2018-01-16 12:44:59 +0100

    Add project files.

---
+++ NeuroEvol/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroEvol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
